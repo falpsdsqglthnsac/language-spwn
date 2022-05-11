d5d3afcc188a39d3804cc028a854bc4904934f2d
falpsdsqglthnsac <62222631+falpsdsqglthnsac@users.noreply.github.com>
2022-05-10 22:41:47 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# language-spwn package
+
+Syntax highlighting package for SPWN, a programming language which compiles to Geometry Dash levels.
+
+[Language repo](https://github.com/Spu7Nix/SPWN-language)
---
+++ spec/language-spwn-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import LanguageSpwnView from '../lib/language-spwn-view';
+
+describe('LanguageSpwnView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
